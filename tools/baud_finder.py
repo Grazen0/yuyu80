be41be108a5ea0fdb8d5544fd59fb5f08133fbc8
José Daniel Grayson <josedanielgrayson@proton.me>
2026-07-28 14:17:24 -0500

    chore: add baud rate finder python script

---
+++ tools/baud_finder.py
@@ -0,0 +1,58 @@
+from dataclasses import dataclass
+
+freq = 4_000_000
+phi = 1 / freq
+
+bauds = [
+    1200,
+    2400,
+    4800,
+    9600,
+    19200,
+    38400,
+    57600,
+    115200,
+]
+
+# TODO: find out why multiplier of 1 doesnt work
+multipliers = [16, 32, 64]
+
+
+def frac(x: float) -> float:
+    return x - int(x)
+
+
+#    phi * 16 * c = 1 / (m * b)
+# => c = 1 / (phi * 16 * m * b)
+
+
+def c(m: int, b: int):
+    return 1 / (phi * 16 * m * b)
+
+
+def f(c: int, m: int, b: int):
+    return phi * 16 * c * m * b
+
+
+@dataclass
+class Combination:
+    b: int
+    m: int
+    c: int
+
+    def error(self):
+        return abs(f(round(self.c), self.m, self.b) - 1)
+
+
+combs = [
+    Combination(b, m, round(c(m, b)))
+    for b in bauds
+    for m in multipliers
+    if round(c(m, b)) != 0
+]
+combs.sort(key=Combination.error)
+
+for comb in combs:
+    print(
+        f"b = {comb.b:6}, m = {comb.m:2} -> c = {comb.c:3}, err = {round(100 * comb.error(), 2):5}%"
+    )
